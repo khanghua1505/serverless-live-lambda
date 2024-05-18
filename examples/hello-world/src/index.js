662b92cec9f489a2a79b8ca247c3da7c21821778
Khang Hua <khanghua1505@gmail.com>
2024-05-18 19:46:00 +0700

    update examples

--- examples/hello-world/src/index.js
+++ examples/hello-world/src/index.js
@@ -4,12 +4,17 @@ const bridge = require('serverless-live-lambda-node-bridge');
 
 exports.handler = bridge(async (event) => {
   const result = {}
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 20000; i++) {
     const randStr = crypto.randomBytes(48).toString('base64')
     result[i] = randStr
   }
+
+  const body = JSON.stringify(result, null, 2);
+  const payloadSize = Math.ceil(body.length / 1024);
+  console.log(`Payload size ${payloadSize} KB`);
+
   return {
     statusCode: 200,
-    body: JSON.stringify(result, null, 2),
+    body,
   }
 });
